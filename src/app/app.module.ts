a045c94951d298578e6b4c1e3a2fecaf7da3b84a
Daniel <danielhernandez002@hotmail.com>
2021-01-14 11:30:31 -0500

    First commit, Added boostrap and material, and users, taskUser model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatListModule } from '@angular/material/list';
+import { UsersListComponent } from './users-list/users-list.component';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    ScrollingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
